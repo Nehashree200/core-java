28d2d78e064b793d0d7c33db3eff15af61775212
Nehashree200 <sainehashree2002@gmail.com>
2024-10-03 10:45:53 +0530

    first code

---
+++ Demo.java
@@ -0,0 +1,40 @@
+public class Demo{
+public static void main(String[] args){
+int age=55;
+int rollno=20;
+int items=8;
+int amount=75;
+int rank=1;
+short distance=500;
+short speed=600;
+short velocity=250;
+short money=300;
+short pricetag=800;
+byte  numberofvehicles=30;
+byte  balance=100;
+byte  numberofbottles=30;
+byte  numberofstudents=80;
+byte  cash=120;
+System.out.println(age);
+System.out.println(rollno);
+System.out.println(items);
+System.out.println(amount);
+System.out.println(rank);
+System.out.println(distance);
+System.out.println(speed);
+System.out.println(velocity);
+System.out.println(money);
+System.out.println(pricetag);
+System.out.println(numberofvehicles);
+System.out.println(balance);
+System.out.println(numberofbottles);
+System.out.println(numberofstudents);
+System.out.println(cash);
+
+
+
+
+
+
+        }
+}
